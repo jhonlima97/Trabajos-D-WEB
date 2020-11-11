ce8088306736a9355029bd3bf41d81c4fa1f3293
jhonlima97 <jhonw.lima08@gmail.com>
2020-11-11 12:46:16 -0500

    commit v5

--- Sesion20/exposicion/ejemplo.js
+++ Sesion20/exposicion/ejemplo.js
@@ -1,14 +1,27 @@
+
+
 function cadenas(){
-    var textos = "Diseño web lima";
+	var textos = "Diseño web";
+	// var textos = ["Diseño web", "Procesos","DAE","Inteligencia Artificial"];
     var contador = textos.length;
 
     document.write(contador)
 }
 
+
+
+
+
+
+
+
+
+
 function buscar(){
     var nombresArray = ["juan", "pedro", "luis", "julia"];
 	var usuario = prompt("Introduce un nombre: ");
-	usuario = usuario.toUpperCase();
+	// usuario = usuario.toUpperCase();
+	// nombresArray.forEach(function(usuario,2,nombresArrays))
 	posicion = nombresArray.indexOf(usuario);
 
 		if(posicion == -1){
