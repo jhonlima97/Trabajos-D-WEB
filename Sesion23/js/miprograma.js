fed8fda5289ecdc9fdd8c59f63b715efc088f07e
jhonlima97 <jhonw.lima08@gmail.com>
2020-11-19 22:06:02 -0500

    commit v7

--- Sesion23/js/miprograma.js
+++ Sesion23/js/miprograma.js
@@ -1,13 +1,13 @@
 function guardarDato(){
     // Capturando Variables
-    const nombre = document.getElementById("nombre").value;
-    const movil = document.getElementById("movil").value;
-    const email = document.getElementById("email").value;
+    var nombre = document.getElementById("nombre").value;
+    var movil = document.getElementById("movil").value;
+    var email = document.getElementById("email").value;
     
-    const datos = {
+    datos = {
         'movil': movil,
         'email': email,
-    }
+    };
     // Almacenando los datos
     localStorage.setItem(nombre, JSON.stringify(datos));
     // Borrando los datos
@@ -20,7 +20,7 @@ function guardarDato(){
 
 function recuperarDato(){
      // Capturando el nombre
-     const nombre = document.getElementById("nombre").value;
+     var nombre = document.getElementById("nombre").value;
      // Parseando los datos
      let datos = localStorage.getItem(nombre);
      datos = JSON.parse(datos);
@@ -30,7 +30,7 @@ function recuperarDato(){
  }
 
 function eliminarDato(){
-    const nombre = document.getElementById("nombre").value;
+    var nombre = document.getElementById("nombre").value;
     localStorage.removeItem(nombre);
     actualizarDatos();
 }
@@ -40,37 +40,21 @@ function eliminarTodo(){
     actualizarDatos();
 }
 
-function actualizarDatos() {
-    const registro = "";
-    // Verificando si existen datos
-    if (localStorage.length === 0) {
-        registro += '<li>Vacío</li>';
-    } else {
-        // Enriqueciendo "elementos" con cada par de valores almacenados
-        for (const i = 0; i <= localStorage.length - 1; i++) {
-            const key = localStorage.key(i);
-            // Parseando los datos 
-            //let datos = localStorage.getItem(key);
+function actualizarDatos(){
+    var registro = "";
+    if(localStorage.length === 0){
+        registro += '<li>Vacío</li>'
+    } else{
+        for(var i = 0; i <= localStorage.length -1; i++){
+            var key = localStorage.key(i);
+            let datos = localStorage.getItem(key);
             datos = JSON.parse(datos);
-            // Enriqueciendo los datos con HTML
+
             registro += '<li>' + '<span class="nom">' + key + '</span>'
-            + '<span class="nom">' + datos.movil + '</span>'
-            + '<span class="nom">' + datos.email + '<li><br>';
-            //<a href="tel:+34${datos.movil}"><p> <span class="movil" ></span> ${datos.movil}</p></a>;
-            //<a href="mailto:${datos.email}"><p> <span class="email" ></span> ${datos.email}</p></a>
-            // Enriqueciendo los datos con HTML
-            //   registro += `<li>
-            //     <div class="col-xs-9">
-            //       <p><b>${key}</b></p>
-            //       <a href="tel:+34${datos.movil}"><p> <span class="movil" ></span> ${datos.movil}</p></a>
-            //       <a href="mailto:${datos.email}"><p> <span class="email" ></span> ${datos.email}</p></a>
-            //     </div>
-            // </div>`;
+            + '<span class="nom">' + datos.movil + '</span>' 
+            + '<span class="nom">' + datos.email + '</span>' 
+            + '</li><br>';
         }
     }
-    // Actualizando "#contactos" con los "elementos" previamente enriquecidos
-    document.getElementById('contactos').innerHTML = elementos;
+    document.getElementById('contactos').innerHTML = registro;
 }
-//actualizarDatos();
-
-            
